65d905a2547f8fc9bf2b52489c5f3559fbbbff6e
Austin Barton <infinitycyc13@gmail.com>
2018-01-16 23:10:47 -0600

    add some images, and include JS to swap through them when the main image is clicked

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,14 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Shovel Knight Fan Page!';
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/shovel-knight-art.jpg') {
+		myImage.setAttribute ('src','images/shovel-of-hope.png');
+	} else if(mySrc === 'images/shovel-of-hope.png') {
+		myImage.setAttribute ('src','images/specter-of-torment.jpg')
+	} else if(mySrc === 'images/specter-of-torment.jpg') {
+		myImage.setAttribute ('src','images/plague-of-shadows.png')
+	} else {
+		myImage.setAttribute ('src','images/shovel-knight-art.jpg')
+	}
+}
