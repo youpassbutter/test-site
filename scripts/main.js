7fe13480f77cd520b0f09b8b48c705184608c6c4
Austin Barton <infinitycyc13@gmail.com>
2018-01-16 23:57:10 -0600

    add personalized welcome message and button to change user

--- scripts/main.js
+++ scripts/main.js
@@ -12,3 +12,18 @@ myImage.onclick = function() {
 		myImage.setAttribute ('src','images/shovel-knight-art.jpg')
 	}
 }
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+	var myName = prompt('Please enter your name:');
+	localStorage.setItem('name', myName);
+	myHeading.textContent = 'Welcome to the Shovel Knight fan page, ' + myName + '!';
+}
+if (!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	var storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Welcome to the Shovel Knight fan page, ' + storedName + '!';
+}
